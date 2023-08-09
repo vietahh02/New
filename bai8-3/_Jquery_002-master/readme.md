12256f284e7cd657a819bc1e3a300e1f420b8419
vietahh02 <72716019+vietahh02@users.noreply.github.com>
2023-08-09 15:51:21 +0700

    Add files via upload

---
+++ bai8-3/_Jquery_002-master/readme.md
@@ -0,0 +1,33 @@
+# Jquery002
+Bài tập thực hành thứ 2 về jquery
+
+## Thời gian thực hiện
+
+**5 - 7 ngày(Part 1: 2 ngày, Part 2: 3 ngày, Part 2 level 2: 2 ngày)**
+
+## Part1 : Áp dụng plugin jquery
+_Sửa lại bài trainee007 với yêu cầu sau_
+
+1. Trang chủ thêm 1 slide chạy hiển thị 5 người dùng mới nhất đã cập nhật ảnh, caption là tên và giới thiệu nếu có. 
+2. Trang danh sách áp dụng https://datatables.net/ vào việc hiển thị danh sách.
+
+**Chú ý trước khi thực hiện: Phải tạo branch mới và làm bài trên branch mới đó, không được ảnh hưởng đến branch `master` bài trainee007 hiện tại, branch mới có tên `jquery002`**
+
+## Part 2 : Sử dụng Jquery tạo slide
+
+Tạo 1 slide với mockup sau
+
+![Normal](Normal.png)
+
+- Level 1: 
+ - Slide chạy từng ảnh 1 (có loop hoặc không loop) với time out 1s, tốc độ 500ms.
+ - Ấn vào nút mũi tên, slide sẽ chạy theo hướng đã chọn ngay lập tức.
+ - Khi hover vào mũi tên, sẽ hiển thị tooltip là thumbnail của slide sẽ chuyển sang sau khi ấn
+
+![Hover](Hover.png)
+
+- Level 2:
+ - Hiển thị vị trí slide đang chạy bằng các chấm tròn phí dưới, slide đang hiển thị ở vị trí bao nhiêu thì chấm tròn đó sẽ là màu khác(có thể là màu trắng).
+ - Có biến để tùy chọn : hiển thị thumnail khi hover nút mũi tên hay không, có hiển thị các chấm tròn biểu thị vị trí slide đang hiển thị hay không, _tùy chọn kiểu chuyển slide_.
+
+![Level 2](Level 2.png)
---
+++ bai8-3/script.js
@@ -0,0 +1,68 @@
+$(() => {
+  let change = 0;
+  const allImg = document.getElementsByClassName("slider-item");
+  $(".dots a").click(function () {
+    $(".dots a").eq(change).removeClass("active");
+    let number = $(this).attr("number");
+    change = number;
+    $(".container .slider .wap").css(
+      "margin-left",
+      -800 * parseInt(number) + "px"
+    );
+    $(".dots a").eq(number).addClass("active");
+    changeThumbnail();
+  });
+  $(".container .thumbnailPrev").click(function () {
+    changeImg("prev");
+  });
+  $(".container .thumbnailNext").click(function () {
+    changeImg("next");
+  });
+  function changeImg(type) {
+    $(".dots a").eq(change).removeClass("active");
+    change = type == "next" ? parseInt(change) + 1 : change - 1;
+    if (change >= allImg.length) {
+      change = 0;
+    }
+    if (change < 0) {
+      change = allImg.length - 1;
+    }
+    $(".container .slider .wap").css(
+      "margin-left",
+      -800 * parseInt(change) + "px"
+    );
+    $(".dots a").eq(change).addClass("active");
+    changeThumbnail();
+  }
+  function changeThumbnail() {
+    prev = change - 1;
+    next = Number(change) + 1;
+    if (change >= allImg.length - 1) {
+      next = 0;
+    }
+    if (change <= 0) {
+      prev = allImg.length - 1;
+    }
+    $(".container .thumbnailPrev div").html(
+      $(".container .slider .wap a").eq(prev).html()
+    );
+    $(".container .thumbnailNext div").html(
+      $(".container .slider .wap a").eq(next).html()
+    );
+    $(".container .button div img").removeClass("slider-item");
+  }
+  $(function () {
+    $(".onThumbnail").click(function () {
+      $(".container .button div").removeClass("offThumb");
+    });
+    $(".offThumbnail").click(function () {
+      $(".container .button div").addClass("offThumb");
+    });
+    $(".onDot").click(function () {
+      $(".dots a").removeClass("offDots");
+    });
+    $(".offDot").click(function () {
+      $(".dots a").addClass("offDots");
+    });
+  });
+});
